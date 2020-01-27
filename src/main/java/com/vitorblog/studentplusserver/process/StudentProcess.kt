4c9302633bd45dafa41a97d2f6c240feaafd7ad2
Vitor <35616041+VitorBlog@users.noreply.github.com>
2020-01-27 18:55:26 -0300

    Update StudentProcess.kt

--- src/main/java/com/vitorblog/studentplusserver/process/StudentProcess.kt
+++ src/main/java/com/vitorblog/studentplusserver/process/StudentProcess.kt
@@ -18,14 +18,15 @@ class StudentProcess {
         val studentInfoResponse = APIRequester.request(Point.GET_STUDENT_INFO, "{\"AlunoCod\": $id}")
         if (studentInfoResponse.equals("{\"ResultadoAluno\":\"[]\"}")) { return null }
         val studentInfoJSON = jsonParser.parseArray(jsonParser.parseJSON(studentInfoResponse).getString("ResultadoAluno")).first() as JSONObject
-
-        val subjects = subjectProcess.load(id, studentInfoJSON.getString("AlunoDataNasc"))
+        
+        val birthday = studentInfoJSON.getString("AlunoDataNasc")
+        val subjects = subjectProcess.load(id, birthday)
         val report = reportProcess.load(id)
         val schedule = scheduleProcess.load(id)
         val menu = menuProcess.load(id)
         val school = School(studentInfoJSON.getString("UeCod"), studentInfoJSON.getString("UeNom").formatName().replace("eeb", "EEB", true), School.Location(studentInfoJSON.getString("UeMunNom"), "${studentInfoJSON.getString("UeEnd").formatName()}, ${studentInfoJSON.getString("UeNumEnd")}", studentInfoJSON.getString("UeLatitude"), studentInfoJSON.getString("UeLongitude")), schedule, menu)
 
-        return Student(id, studentInfoJSON.getString("AlunoNom").formatName(), studentInfoJSON.getString("AlunoDataNasc"), subjects, report, school)
+        return Student(id, studentInfoJSON.getString("AlunoNom").formatName(), birthday, subjects, report, school)
     }
 
     fun loadMinimal(id:String):StudentMini?{
