c77e7879123fbfd9e99911c07005e601953e6bae
vitor <vitorblog12@gmail.com>
2020-04-01 10:51:16 -0300

    Resolving StringLiteralDuplication issue

--- src/main/kotlin/com/vitorblog/studentplusserver/process/ReportProcess.kt
+++ src/main/kotlin/com/vitorblog/studentplusserver/process/ReportProcess.kt
@@ -1,17 +1,17 @@
 package com.vitorblog.studentplusserver.process
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.vitorblog.studentplusserver.model.*
+import com.vitorblog.studentplusserver.model.system.Point
+import com.vitorblog.studentplusserver.model.teacher.Report
+import com.vitorblog.studentplusserver.model.teacher.ReportGrade
 import com.vitorblog.studentplusserver.requester.APIRequester
 import com.vitorblog.studentplusserver.utils.StringUtils
 import org.json.simple.JSONObject
 import org.json.simple.parser.JSONParser
-import java.text.SimpleDateFormat
 
 class ReportProcess {
     val jsonParser = JSONParser()
 
-    fun load(id:String):Report {
+    fun load(id:String): Report {
         var first = arrayListOf<ReportGrade>()
         var second = arrayListOf<ReportGrade>()
         var third = arrayListOf<ReportGrade>()
@@ -23,10 +23,16 @@ class ReportProcess {
             val subject = sub as JSONObject
 
             for (i in 1..3){
+                val reportGrade = ReportGrade(
+                    StringUtils.formatName(subject.getString("DisciplinaNome")),
+                    subject.getLong("Nota$i").toDouble(),
+                    subject.getLong("Falta$i").toInt()
+                )
+
                 when (i) {
-                    1 -> first.add(ReportGrade(StringUtils.formatName(subject.getString("DisciplinaNome")), subject.getLong("Nota$i").toDouble(), subject.getLong("Falta$i").toInt()))
-                    2 -> second.add(ReportGrade(StringUtils.formatName(subject.getString("DisciplinaNome")), subject.getLong("Nota$i").toDouble(), subject.getLong("Falta$i").toInt()))
-                    3 -> third.add(ReportGrade(StringUtils.formatName(subject.getString("DisciplinaNome")), subject.getLong("Nota$i").toDouble(), subject.getLong("Falta$i").toInt()))
+                    1 -> first.add(reportGrade)
+                    2 -> second.add(reportGrade)
+                    3 -> third.add(reportGrade)
                 }
             }
         }
